aa6358c48ffd9e4d0ffc10c00be17117838413c6
Nikita <kuzliaev.nikita@yandex.ru>
2025-10-06 13:26:21 +0700

    feat (closes #2): add `PostgreSQL` connection, test database-check endpoint (№3) is working now

---
+++ backend/TrashBoard.Infrastructure/Migrations/20251006061025_InitialCreate.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace TrashBoard.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "threads",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Description = table.Column<string>(type: "character varying(1000)", maxLength: 1000, nullable: false),
+                    CreatorId = table.Column<int>(type: "integer", nullable: false),
+                    visibility = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_threads", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "boards",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Description = table.Column<string>(type: "character varying(1000)", maxLength: 1000, nullable: false),
+                    ThreadId = table.Column<int>(type: "integer", nullable: false),
+                    visibility = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_boards", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_boards_threads_ThreadId",
+                        column: x => x.ThreadId,
+                        principalTable: "threads",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "pages",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Content = table.Column<string>(type: "character varying(1000)", maxLength: 1000, nullable: false),
+                    BoardId = table.Column<int>(type: "integer", nullable: false),
+                    visibility = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_pages", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_pages_boards_BoardId",
+                        column: x => x.BoardId,
+                        principalTable: "boards",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_boards_ThreadId",
+                table: "boards",
+                column: "ThreadId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_pages_BoardId",
+                table: "pages",
+                column: "BoardId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "pages");
+
+            migrationBuilder.DropTable(
+                name: "boards");
+
+            migrationBuilder.DropTable(
+                name: "threads");
+        }
+    }
+}
