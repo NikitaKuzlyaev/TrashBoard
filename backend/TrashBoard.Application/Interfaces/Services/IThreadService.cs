82569b42ca3ed2d05b6e91f4ed1606f84ccfe820
Nikita <kuzliaev.nikita@yandex.ru>
2025-10-09 21:40:33 +0700

    feat: add `ThreadService` interface and realization with some methods

---
+++ backend/TrashBoard.Application/Interfaces/Services/IThreadService.cs
@@ -0,0 +1,14 @@
+﻿namespace TrashBoard.Application.Interfaces.Services
+{
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using TrashBoard.Application.ResultModels;
+    using TrashBoard.Domain.ValueObjects;
+
+    public interface IThreadService
+    {
+        Task<List<ThreadResult>> GetLatestThreadsAsync(int count = 10);
+        Task<ThreadResult?> GetThreadByIdAsync(int threadId);
+        Task<ThreadResult> CreateThreadAsync(string name, string description, int creatorId, Visibility visibility);
+    }
+}
---
+++ backend/TrashBoard.Application/Services/ThreadService.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrashBoard.Application.Interfaces;
+using TrashBoard.Application.Interfaces.Services;
+using TrashBoard.Application.ResultModels;
+using TrashBoard.Domain.Entities;
+using TrashBoard.Domain.ValueObjects;
+using Thread = TrashBoard.Domain.Entities.Thread;
+
+namespace TrashBoard.Application.Services
+{
+    public class ThreadService : IThreadService
+    {
+        private readonly IThreadRepository _threadRepo;
+
+        public ThreadService(IThreadRepository threadRepo)
+        {
+            _threadRepo = threadRepo;
+        }
+
+        public async Task<List<ThreadResult>> GetLatestThreadsAsync(int count = 10)
+        {
+            var threads = await _threadRepo.GetLatestThreadsAsync(count);
+
+            return threads.Select(t => new ThreadResult
+            {
+                Id = t.Id,
+                Name = t.Name,
+                CreatedAt = t.CreatedAt
+            }).ToList();
+        }
+
+        public async Task<ThreadResult?> GetThreadByIdAsync(int threadId)
+        {
+            var thread = await _threadRepo.GetByIdAsync(threadId);
+            if (thread == null) return null;
+
+            return new ThreadResult
+            {
+                Id = thread.Id,
+                Name = thread.Name,
+                CreatedAt = thread.CreatedAt
+            };
+        }
+
+        public async Task<ThreadResult> CreateThreadAsync(string name, string description, int creatorId, Visibility visibility)
+        {
+            var thread = new Thread(name, description, creatorId, visibility);
+
+            await _threadRepo.AddAsync(thread);
+
+            return new ThreadResult
+            {
+                Id = thread.Id,
+                Name = thread.Name,
+                CreatedAt = thread.CreatedAt
+            };
+        }
+
+    }
+
+    
+
+}
